948bcabed80213b021228b4782af80f9df3018a3
“yangfengbang” <yangfengbang@zplay.cn>
2019-11-01 16:21:12 +0800

    add autoLoad，channelId，requestReadPhoneState，gdprState admob server params

--- admobadapter/src/main/java/com/yumiad/admobadapter/PlayableAdRewardedVideo.java
+++ admobadapter/src/main/java/com/yumiad/admobadapter/PlayableAdRewardedVideo.java
@@ -1,16 +1,18 @@
 package com.yumiad.admobadapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.mediation.MediationAdRequest;
 import com.google.android.gms.ads.reward.mediation.MediationRewardedVideoAdAdapter;
 import com.google.android.gms.ads.reward.mediation.MediationRewardedVideoAdListener;
 import com.playableads.PlayLoadingListener;
 import com.playableads.PlayPreloadingListener;
 import com.playableads.PlayableAds;
+import com.playableads.PlayableAdsSettings;
 
 /**
  * Description: PlayableAdRewardedVideo
@@ -20,36 +22,54 @@ import com.playableads.PlayableAds;
 @SuppressWarnings("unused")
 public class PlayableAdRewardedVideo implements MediationRewardedVideoAdAdapter {
     private static final String TAG = "PlayableAdRewardedVideo";
-    private String paAppId;
-    private String paAdUnitId;
-    private PlayableAds pAd;
+    private PlayableAds playableAds;
     private MediationRewardedVideoAdListener mRewardedVideoEventForwarder;
-
+    private PlayableUtil.PlayableParams params;
     @Override
     public void initialize(Context context, MediationAdRequest mediationAdRequest, String s, MediationRewardedVideoAdListener mediationRewardedVideoAdListener, Bundle serverParameters, Bundle bundle1) {
-        resetIds(serverParameters);
-        pAd = PlayableAds.init(context, paAppId);
-        pAd.setAutoLoadAd(false);
-        pAd.enableAutoRequestPermissions(true);
+        Log.d(TAG, "initialize");
+        if (!(context instanceof Activity)) {
+            Log.e(TAG, "requestInterstitialAd: PlayableAd needs Activity object to initialize sdk.");
+            mediationRewardedVideoAdListener.onAdFailedToLoad(this, AdRequest.ERROR_CODE_INVALID_REQUEST);
+            return;
+        }
+
+        params = new PlayableUtil.PlayableParams(serverParameters.getString(CUSTOM_EVENT_SERVER_PARAMETER_FIELD));
+
+        Log.d(TAG, "requestReadPhoneState: " + params.requestReadPhoneState);
+        PlayableAdsSettings.enableAutoRequestPermissions(params.requestReadPhoneState);
+        Log.d(TAG, "gdprState: " + params.gdprState);
+        PlayableUtil.setGDPRConsent(params.gdprState);
+
+        playableAds = PlayableAds.init(context, params.appId);
+        playableAds.setAutoLoadAd(params.autoLoad);
+        playableAds.setChannelId(params.channelId);
+        playableAds.enableAutoRequestPermissions(params.requestReadPhoneState);
         mRewardedVideoEventForwarder = mediationRewardedVideoAdListener;
         mRewardedVideoEventForwarder.onInitializationSucceeded(PlayableAdRewardedVideo.this);
     }
 
     @Override
     public void loadAd(MediationAdRequest mediationAdRequest, Bundle serverParameters, Bundle bundle1) {
-        resetIds(serverParameters);
+        if (playableAds == null) {
+            Log.e(TAG, "PlayableAd not initialized.");
+            return;
+        }
         loadAd();
     }
 
     private void loadAd() {
-        pAd.requestPlayableAds(paAdUnitId, new PlayPreloadingListener() {
+        Log.d(TAG, "requestRewardedVideoAd");
+        playableAds.requestPlayableAds(params.unitId, new PlayPreloadingListener() {
             @Override
             public void onLoadFinished() {
+                Log.d(TAG, "onLoadFinished");
                 mRewardedVideoEventForwarder.onAdLoaded(PlayableAdRewardedVideo.this);
             }
 
             @Override
-            public void onLoadFailed(int i, String s) {
+            public void onLoadFailed(int code, String errorMsg) {
+                Log.e(TAG, "onLoadFailed code: " + code + ", errorMsg: " + errorMsg);
                 mRewardedVideoEventForwarder.onAdFailedToLoad(PlayableAdRewardedVideo.this, 0);
             }
         });
@@ -57,35 +77,41 @@ public class PlayableAdRewardedVideo implements MediationRewardedVideoAdAdapter 
 
     @Override
     public void showVideo() {
-        if (pAd.canPresentAd(paAdUnitId)) {
+        if (playableAds.canPresentAd(params.unitId)) {
             mRewardedVideoEventForwarder.onAdOpened(PlayableAdRewardedVideo.this);
-            pAd.presentPlayableAD(paAdUnitId, new PlayLoadingListener() {
+            playableAds.presentPlayableAD(params.unitId, new PlayLoadingListener() {
 
                 @Override
                 public void onVideoStart() {
+                    Log.d(TAG, "onVideoStart");
                     mRewardedVideoEventForwarder.onVideoStarted(PlayableAdRewardedVideo.this);
                 }
 
                 @Override
                 public void onVideoFinished() {
+                    Log.d(TAG, "onVideoFinished");
                     mRewardedVideoEventForwarder.onVideoCompleted(PlayableAdRewardedVideo.this);
                 }
 
                 @Override
                 public void onLandingPageInstallBtnClicked() {
+                    Log.d(TAG, "onLandingPageInstallBtnClicked");
                     mRewardedVideoEventForwarder.onAdClicked(PlayableAdRewardedVideo.this);
                 }
 
                 public void playableAdsIncentive() {
+                    Log.d(TAG, "playableAdsIncentive");
                     mRewardedVideoEventForwarder.onRewarded(PlayableAdRewardedVideo.this, null);
                 }
 
                 @Override
                 public void onAdClosed() {
+                    Log.d(TAG, "onAdClosed");
                     mRewardedVideoEventForwarder.onAdClosed(PlayableAdRewardedVideo.this);
                 }
 
                 public void onAdsError(int var1, String var2) {
+                    Log.e(TAG, "present onAdsError code: " + var1 + ", errorMsg: " + var2);
                     mRewardedVideoEventForwarder.onAdFailedToLoad(PlayableAdRewardedVideo.this, 0);
                 }
 
@@ -93,24 +119,9 @@ public class PlayableAdRewardedVideo implements MediationRewardedVideoAdAdapter 
         }
     }
 
-    private void resetIds(@NonNull Bundle param) {
-        String parameters = param.getString("parameter");
-        if (parameters == null) {
-            Log.e(TAG, "check parameter from AdMob web");
-            return;
-        }
-        String[] ids = parameters.trim().split("\\s");
-        if (ids.length != 2) {
-            Log.e(TAG, "check parameter from AdMob web");
-            return;
-        }
-        paAppId = ids[0];
-        paAdUnitId = ids[1];
-    }
-
     @Override
     public boolean isInitialized() {
-        return pAd != null;
+        return playableAds != null;
     }
 
     @Override
